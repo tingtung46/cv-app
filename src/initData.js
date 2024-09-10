a95073e0240a3494e37d0af581eb52b3f0b8d097
root <132725436+tingtung46@users.noreply.github.com>
2024-09-10 11:05:20 +0700

    Add init data

---
+++ src/initData.js
@@ -0,0 +1,18 @@
+export const emptyData = {
+  generalInfo: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    location: "",
+  },
+  education: [],
+  experience: [],
+  projects: [],
+  technicalSkills: {
+    languages: "",
+    frameworks: "",
+    devTools: "",
+    libraries: "",
+  },
+};
