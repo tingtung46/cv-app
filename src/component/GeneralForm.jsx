f970f42b6dca175a00f6d536df80637a96b45aa2
root <132725436+tingtung46@users.noreply.github.com>
2024-09-10 07:16:36 +0700

    Remove button and form tag

--- src/component/GeneralForm.jsx
+++ src/component/GeneralForm.jsx
@@ -1,5 +1,4 @@
 import Section from "./Section";
-import Button from "./Button";
 
 export default function GeneralInfoForm({ generalInfo, setGeneralInfo }) {
   const handleFirstName = (e) => {
@@ -22,72 +21,59 @@ export default function GeneralInfoForm({ generalInfo, setGeneralInfo }) {
     setGeneralInfo({ ...generalInfo, location: e.target.value });
   };
 
-  const handleNext = (e) => {
-    e.preventDefault();
-  };
-
   return (
     <Section title="General Information">
       <>
-        <form action="#">
-          <div className="full-name">
-            <div>
-              <label htmlFor="first-name">First Name</label>
-              <input
-                type="text"
-                name="first-name"
-                id="first-name"
-                onChange={handleFirstName}
-              />
-            </div>
-            <div>
-              <label htmlFor="last-name">Last Name</label>
-              <input
-                type="text"
-                name="last-name"
-                id="last-name"
-                onChange={handleLastName}
-              />
-            </div>
-          </div>
-
+        <div className="full-name">
           <div>
-            <label htmlFor="email-address">Email</label>
-            <input
-              type="email"
-              name="email-address"
-              id="email-address"
-              onChange={handleEmail}
-            />
-          </div>
-
-          <div>
-            <label htmlFor="phone-number">Phone Number</label>
-            <input
-              type="tel"
-              name="phone-number"
-              id="phone-number"
-              onChange={handlePhoneNumber}
-            />
-          </div>
-
-          <div>
-            <label htmlFor="location">Location</label>
+            <label htmlFor="first-name">First Name</label>
             <input
               type="text"
-              name="location"
-              id="location"
-              onChange={handleLocation}
+              name="first-name"
+              id="first-name"
+              onChange={handleFirstName}
             />
           </div>
+          <div>
+            <label htmlFor="last-name">Last Name</label>
+            <input
+              type="text"
+              name="last-name"
+              id="last-name"
+              onChange={handleLastName}
+            />
+          </div>
+        </div>
 
-          <Button
-            type="submit"
-            onClick={handleNext}
-            className="next-btn"
-            text="Next"
-          ></Button>
-        </form>
+        <div>
+          <label htmlFor="email-address">Email</label>
+          <input
+            type="email"
+            name="email-address"
+            id="email-address"
+            onChange={handleEmail}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="phone-number">Phone Number</label>
+          <input
+            type="tel"
+            name="phone-number"
+            id="phone-number"
+            onChange={handlePhoneNumber}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="location">Location</label>
+          <input
+            type="text"
+            name="location"
+            id="location"
+            onChange={handleLocation}
+          />
+        </div>
       </>
     </Section>
   );
