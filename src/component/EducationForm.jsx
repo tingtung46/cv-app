4001bf2d1041ea232caeea77f2499cefe372e79d
root <132725436+tingtung46@users.noreply.github.com>
2024-09-16 10:07:39 +0700

    Change a way how to add new education

--- src/component/EducationForm.jsx
+++ src/component/EducationForm.jsx
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
+import EducationInput from "./EducationInput";
 import Button from "./Button";
 import Section from "./Section";
 
@@ -8,112 +9,33 @@ export default function EducationForm({ resumeData, setResumeData }) {
 
     setResumeData({
       ...resumeData,
-      education: [
-        ...resumeData.education,
-        {
-          id: newID,
-          schoolName: "",
-          schoolLocation: "",
-          major: "",
-          startDate: "",
-          endDate: "",
-        },
-      ],
+      education: [...resumeData.education, [newID]],
+      [newID]: {
+        schoolName: "",
+        schoolLocation: "",
+        major: "",
+        startDate: "",
+        endDate: "",
+      },
     });
   };
 
-  const handleSchoolName = (e) => {
-    setResumeData({
-      ...resumeData,
-      education: [...resumeData.education, { schoolName: e.target.value }],
-    });
-  };
-
-  const handleSchoolLocation = (e) => {
-    setResumeData({
-      ...resumeData,
-      education: [...resumeData.education, { schoolLocation: e.target.value }],
-    });
-  };
-
-  const handleMajor = (e) => {
-    setResumeData({
-      ...resumeData,
-      education: [...resumeData.education, { major: e.target.value }],
-    });
-  };
-
-  const handleStartDate = (e) => {
-    setResumeData({
-      ...resumeData,
-      education: [...resumeData.education, { startDate: e.target.value }],
-    });
-  };
-
-  const handleEndDate = (e) => {
-    setResumeData({
-      ...resumeData,
-      education: [...resumeData.education, { endDate: e.target.value }],
-    });
+  const handleChange = (newData) => {
+    setResumeData(newData);
   };
 
   return (
     <>
       <Section title="Education">
         {resumeData.education.map((item) => {
-          <div className="education-inp" key={item.id}>
-            <div>
-              <label htmlFor="school-name">School Name</label>
-              <input
-                type="text"
-                name="school-name"
-                id="school-name"
-                onChange={handleSchoolName}
-              />
-            </div>
-
-            <div>
-              <label htmlFor="school-location">School Location</label>
-              <input
-                type="text"
-                name="school-location"
-                id="school-location"
-                onChange={handleSchoolLocation}
-              />
-            </div>
-
-            <div>
-              <label htmlFor="major">Major</label>
-              <input
-                type="text"
-                name="major"
-                id="major"
-                onChange={handleMajor}
-              />
-            </div>
-
-            <div className="school-date">
-              <div>
-                <label htmlFor="school-start-date">From</label>
-                <input
-                  type="text"
-                  name="school-start-date"
-                  id="school-start-date"
-                  onChange={handleStartDate}
-                />
-              </div>
-
-              <div>
-                <label htmlFor="school-start-end">To</label>
-                <input
-                  type="text"
-                  name="school-start-end"
-                  id="school-start-end"
-                  onChange={handleEndDate}
-                />
-              </div>
-            </div>
-          </div>;
+          return (
+            <EducationInput
+              key={item}
+              itemID={item}
+              handleChange={handleChange}
+              resumeData={resumeData}
+            />
+          );
         })}
 
         <Button
