b537c69e76d52f774a63edede22c3cf31fe3cbfb
root <132725436+tingtung46@users.noreply.github.com>
2024-09-18 14:02:20 +0700

    Add experience input component

---
+++ src/component/ExperienceInput.jsx
@@ -0,0 +1,145 @@
+import { Fragment } from "react";
+import Button from "./Button";
+import { v4 as uuidv4 } from "uuid";
+
+export default function ExperienceInput({ itemID, handleChange, resumeData }) {
+  const handleResponsibility = () => {
+    const newID = uuidv4();
+
+    handleChange({
+      ...resumeData,
+      [itemID]: {
+        ...resumeData[itemID],
+        responsibility: [...resumeData[itemID]["responsibility"], [newID]],
+        [newID]: "",
+      },
+    });
+  };
+
+  return (
+    <Fragment>
+      <div className="experience-input">
+        <div className="input">
+          <label htmlFor="company-name">Company Name</label>
+          <input
+            type="text"
+            name="company-name"
+            id="company-name"
+            onChange={(e) => {
+              handleChange({
+                ...resumeData,
+                [itemID]: {
+                  ...resumeData[itemID],
+                  companyName: e.target.value,
+                },
+              });
+            }}
+          />
+        </div>
+
+        <div className="input">
+          <label htmlFor="company-location">Location</label>
+          <input
+            type="text"
+            name="company-location"
+            id="company-location"
+            onChange={(e) => {
+              handleChange({
+                ...resumeData,
+                [itemID]: {
+                  ...resumeData[itemID],
+                  location: e.target.value,
+                },
+              });
+            }}
+          />
+        </div>
+
+        <div className="input">
+          <label htmlFor="position-title">Position Title</label>
+          <input
+            type="text"
+            name="position-title"
+            id="position-title"
+            onChange={(e) => {
+              handleChange({
+                ...resumeData,
+                [itemID]: {
+                  ...resumeData[itemID],
+                  positionTitle: e.target.value,
+                },
+              });
+            }}
+          />
+        </div>
+
+        <div className="company-date">
+          <div className="input">
+            <label htmlFor="company-start-date">From</label>
+            <input
+              type="text"
+              name="company-start-date"
+              id="company-start-date"
+              onChange={(e) => {
+                handleChange({
+                  ...resumeData,
+                  [itemID]: {
+                    ...resumeData[itemID],
+                    startDate: e.target.value,
+                  },
+                });
+              }}
+            />
+          </div>
+
+          <div className="input">
+            <label htmlFor="company-end-date">To</label>
+            <input
+              type="text"
+              name="company-end-date"
+              id="company-end-date"
+              onChange={(e) => {
+                handleChange({
+                  ...resumeData,
+                  [itemID]: { ...resumeData[itemID], endDate: e.target.value },
+                });
+              }}
+            />
+          </div>
+        </div>
+
+        <div className="input">
+          <label htmlFor="responsibility">Responsibility</label>
+          <ul>
+            {resumeData[itemID]["responsibility"]["map"]((item) => {
+              return (
+                <>
+                  <li key={item}>
+                    <input
+                      type="text"
+                      onChange={(e) => [
+                        handleChange({
+                          ...resumeData,
+                          [itemID]: {
+                            ...resumeData[itemID],
+                            [item]: e.target.value,
+                          },
+                        }),
+                      ]}
+                    />
+                  </li>
+                </>
+              );
+            })}
+          </ul>
+          <Button
+            type="button"
+            onClick={handleResponsibility}
+            className="responsibility-btn"
+            text="Add Responsibility"
+          />
+        </div>
+      </div>
+    </Fragment>
+  );
+}
