1be93dee7b2392792adc5ab13f9979d7dd546d1e
root <132725436+tingtung46@users.noreply.github.com>
2024-09-21 08:45:51 +0700

    Add logic for each input

--- src/component/ProjectInput.jsx
+++ src/component/ProjectInput.jsx
@@ -1,35 +1,131 @@
-export default function ProjectInput() {
+import { v4 as uuidv4 } from "uuid";
+import { Fragment } from "react";
+import Button from "./Button";
+
+export default function ProjectInput({ itemID, handleChange, resumeData }) {
+  const handleDescription = () => {
+    const newID = uuidv4();
+
+    handleChange({
+      ...resumeData,
+      [itemID]: {
+        ...resumeData[itemID],
+        description: [...resumeData[itemID]["description"], [newID]],
+        [newID]: "",
+      },
+    });
+  };
+
   return (
     <>
-      <div className="project-input">
-        <div className="input">
-          <label htmlFor="project-name">Project Name</label>
-          <input type="text" name="project-name" id="project-name" />
-        </div>
-
-        <div className="input">
-          <label htmlFor="project-tools">Tools</label>
-          <input type="text" name="project-tools" id="project-tools" />
-        </div>
-
-        <div className="project-date">
+      <Fragment>
+        <div className="project-input">
           <div className="input">
-            <label htmlFor="project-start-date">From</label>
+            <label htmlFor="project-name">Project Name</label>
             <input
               type="text"
-              name="project-start-date"
-              id="project-start-date"
+              name="project-name"
+              id="project-name"
+              onChange={(e) => {
+                handleChange({
+                  ...resumeData,
+                  [itemID]: {
+                    ...resumeData[itemID],
+                    projectName: e.target.value,
+                  },
+                });
+              }}
             />
           </div>
 
           <div className="input">
-            <label htmlFor="project-end-date">To</label>
-            <input type="text" name="project-end-date" id="project-end-date" />
+            <label htmlFor="project-tools">Tools</label>
+            <input
+              type="text"
+              name="project-tools"
+              id="project-tools"
+              onChange={(e) => {
+                handleChange({
+                  ...resumeData,
+                  [itemID]: {
+                    ...resumeData[itemID],
+                    tools: e.target.value,
+                  },
+                });
+              }}
+            />
+          </div>
+
+          <div className="project-date">
+            <div className="input">
+              <label htmlFor="project-start-date">From</label>
+              <input
+                type="text"
+                name="project-start-date"
+                id="project-start-date"
+                onChange={(e) => {
+                  handleChange({
+                    ...resumeData,
+                    [itemID]: {
+                      ...resumeData[itemID],
+                      startDate: e.target.value,
+                    },
+                  });
+                }}
+              />
+            </div>
+
+            <div className="input">
+              <label htmlFor="project-end-date">To</label>
+              <input
+                type="text"
+                name="project-end-date"
+                id="project-end-date"
+                onChange={(e) => {
+                  handleChange({
+                    ...resumeData,
+                    [itemID]: {
+                      ...resumeData[itemID],
+                      endDate: e.target.value,
+                    },
+                  });
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="input">
+            <label htmlFor="project-description">Description</label>
+            <ul>
+              {resumeData[itemID]["description"]["map"]((item) => {
+                return (
+                  <li key={item}>
+                    <input
+                      type="text"
+                      onChange={(e) => {
+                        handleChange({
+                          ...resumeData,
+                          [itemID]: {
+                            ...resumeData[itemID],
+                            [item]: e.target.value,
+                          },
+                        });
+                      }}
+                    />
+                  </li>
+                );
+              })}
+            </ul>
+
+            <Button
+              type="button"
+              onClick={handleDescription}
+              className="description-btn"
+              text="Add Description"
+            />
           </div>
         </div>
-
-        <div className="project-description"></div>
-      </div>
+      </Fragment>
     </>
   );
 }
