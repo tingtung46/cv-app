4de9e14447455d5d420e481f933a5cab086892ef
root <132725436+tingtung46@users.noreply.github.com>
2024-09-22 11:51:53 +0700

    Add skill form component

---
+++ src/component/SkillsForm.jsx
@@ -0,0 +1,89 @@
+import Section from "./Section";
+
+export default function SkillsForm({ resumeData, setResumeData }) {
+  const handleLanguages = (e) => {
+    setResumeData({
+      ...resumeData,
+      technicalSkills: {
+        ...resumeData.technicalSkills,
+        languages: e.target.value,
+      },
+    });
+  };
+
+  const handleFrameworks = (e) => {
+    setResumeData({
+      ...resumeData,
+      technicalSkills: {
+        ...resumeData.technicalSkills,
+        frameworks: e.target.value,
+      },
+    });
+  };
+
+  const handleDevTools = (e) => {
+    setResumeData({
+      ...resumeData,
+      technicalSkills: {
+        ...resumeData.technicalSkills,
+        devTools: e.target.value,
+      },
+    });
+  };
+
+  const handleLibraries = (e) => {
+    setResumeData({
+      ...resumeData,
+      technicalSkills: {
+        ...resumeData.technicalSkills,
+        libraries: e.target.value,
+      },
+    });
+  };
+
+  return (
+    <>
+      <Section title="Technical Skills">
+        <div className="input">
+          <label htmlFor="program-languages">Program Languages</label>
+          <input
+            type="text"
+            name="program-languages"
+            id="program-languages"
+            onChange={handleLanguages}
+          />
+        </div>
+
+        <div className="input">
+          <label htmlFor="frameworks">Frameworks</label>
+          <input
+            type="text"
+            name="frameworks"
+            id="frameworks"
+            onChange={handleFrameworks}
+          />
+        </div>
+
+        <div className="input">
+          <label htmlFor="dev-tools">Developer Tools</label>
+          <input
+            type="text"
+            name="dev-tools"
+            id="dev-tools"
+            onChange={handleDevTools}
+          />
+        </div>
+
+        <div className="input">
+          <label htmlFor="libraries">Libraries</label>
+          <input
+            type="text"
+            name="libraries"
+            id="libraries"
+            onChange={handleLibraries}
+          />
+        </div>
+      </Section>
+    </>
+  );
+}
