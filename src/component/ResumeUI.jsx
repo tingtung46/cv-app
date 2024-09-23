fc977bdecc20a6a9449656dd81f5e6ebcf52c2f2
root <132725436+tingtung46@users.noreply.github.com>
2024-09-23 12:55:03 +0700

    Add education UI component

--- src/component/ResumeUI.jsx
+++ src/component/ResumeUI.jsx
@@ -14,6 +14,28 @@ export default function ResumeUI({ resumeData }) {
             {resumeData.generalInfo.location}
           </p>
         </section>
+
+        <section className="ui education">
+          <h2>Education</h2>
+          {resumeData.education.map((item) => {
+            return (
+              <div className="school-main" key={item}>
+                <div className="school-desc">
+                  <h3>{resumeData[item]["schoolName"]}</h3>
+                  <p>
+                    {resumeData[item]["startDate"]} -{" "}
+                    {resumeData[item]["endDate"]}
+                  </p>
+                </div>
+
+                <p className="school-location">
+                  {resumeData[item]["schoolLocation"]}
+                </p>
+                <p>{resumeData[item]["major"]}</p>
+              </div>
+            );
+          })}
+        </section>
       </main>
     </>
   );
