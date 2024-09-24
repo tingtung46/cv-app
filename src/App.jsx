1e71df4ff0d428d95e000be4597cb4264db8dd87
root <132725436+tingtung46@users.noreply.github.com>
2024-09-24 11:17:33 +0700

    Create root and add main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import GeneralInfoForm from "./component/GeneralForm";
+import EducationalForm from "./component/EducationForm";
+import ExperienceForm from "./component/ExperienceForm";
+import ProjectForm from "./component/ProjectForm";
+import SkillsForm from "./component/SkillsForm";
+import ResumeUI from "./component/ResumeUI";
+import { emptyData } from "./initData";
+import Button from "./component/Button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [resumeData, setResumeData] = useState(emptyData);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  if (isSubmitted === false) {
+    return (
+      <>
+        <main>
+          <GeneralInfoForm
+            resumeData={resumeData}
+            setResumeData={setResumeData}
+          />
+          <EducationalForm
+            resumeData={resumeData}
+            setResumeData={setResumeData}
+          />
+          <ExperienceForm
+            resumeData={resumeData}
+            setResumeData={setResumeData}
+          />
+          <ProjectForm resumeData={resumeData} setResumeData={setResumeData} />
+          <SkillsForm resumeData={resumeData} setResumeData={setResumeData} />
+
+          <Button
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              setIsSubmitted(true);
+            }}
+            className="submit-btn"
+            text="Create Resume"
+          />
+        </main>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <ResumeUI resumeData={resumeData} />
+        <Button
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            setIsSubmitted(false);
+          }}
+          className="edit-btn"
+          text="Edit Resume"
+        />
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
